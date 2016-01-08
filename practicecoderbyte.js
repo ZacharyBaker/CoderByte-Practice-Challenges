87b440e1a253c7350d836f50875ea6d92e92a394
Zach Baker <zach.r.baker1@gmail.com>
2016-01-08 11:52:21 -0700

    correct answer triangle type without calc angles

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1115,3 +1115,13 @@ function triangleType(a, b, c){
 
   return 0;
 }
+//-----------------
+
+function triangleType(a, b, c){
+  var max = Math.max(a,b,c);
+  
+  if (a+b+c <= 2*max) return 0;
+  if (2*max*max == a*a+b*b+c*c) return 2;
+  if (2*max*max >  a*a+b*b+c*c) return 3;
+  return 1;
+}
