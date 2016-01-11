85b2f851917d2f2e136fc9297f61df7ea0e1ff98
Zach Baker <zach.r.baker1@gmail.com>
2016-01-10 22:01:43 -0700

    nameProbFunc

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1161,3 +1161,13 @@ function isPrime(value) {
     }
     return value > 1;
 }
+
+//------------------------
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names 
+// separated by commas except for the last two names,
+//  which should be separated by an ampersand.
+function list(names){
+  //your code here
+}
