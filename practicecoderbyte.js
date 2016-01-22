c577cca6f6b658c906b18e70e3743348028367ce
Zach Baker <zach.r.baker1@gmail.com>
2016-01-22 09:14:39 -0700

    javasauce

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1318,3 +1318,18 @@ function countArara(n) {
 	
 	else return arara;
 }
+
+//-------------------
+//Write a function that takes an array and replaces all of the odd numbers with the letter ‘O’.
+
+var replace = function (arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 !== 0) {
+			arr[i] = 'O';
+		}
+	}
+	return arr;
+}
+
+var myArray = [1, 2, 3, 4, 5, 6];
+replace(myArray);
