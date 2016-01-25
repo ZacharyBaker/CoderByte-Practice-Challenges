3bc60c079ccc51b394ef3be5767c8291f15cee54
Zach Baker <zach.r.baker1@gmail.com>
2016-01-25 08:30:46 -0700

    progress on dictionaryFn

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1369,3 +1369,31 @@ function simpleSymbols(str) {
 	}
 	return value;
 }
+//-------------------------------
+
+//In this kata you have to extend the dictionary with a method, 
+//that returns a list of words matching a pattern. This pattern
+// may contain letters (lowercase) and placeholders ("?"). A placeholder 
+//stands for exactly one arbitrary letter.
+
+function Dictionary(words) {
+  this.words = words;
+}
+
+Dictionary.prototype.getMatchingWords = function(pattern) {
+  var words = Dictionary.words;
+  words.forEach(function(e){
+    var addWord = false;
+    for (var i = 0; i < pattern.length; i++){
+      if (pattern[i] === "?" && e[i]){
+        addWord = true;
+      
+      }
+      else if (pattern[i] !== "?"){
+        if (pattern[i] === e[i]) addWord = true;
+        else return false;
+      }
+    }
+  })
+    
+}
