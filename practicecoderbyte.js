d675c5382190068033e13b6413c6c218d605c7ed
Zach Baker <zach.r.baker1@gmail.com>
2016-02-01 10:10:02 -0700

    emailValidation

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1423,3 +1423,10 @@ function validatePin(pin) {
 	var x = pin.replace(/[^0-9]/g, "");
 	return x.length === pin.length;
 }
+
+//-------------------
+//testEmail
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
