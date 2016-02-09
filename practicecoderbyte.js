42c2227fc3cf74498acbbe5f6167b33bc414c5b7
Zach Baker <zach.r.baker1@gmail.com>
2016-02-08 20:37:39 -0700

    decipher code

--- practicecoderbyte.js
+++ practicecoderbyte.js
@@ -1496,7 +1496,71 @@ var lastDigit3 = function(str1, str2){
 //rot13 decipher the joke
 
 function rot13(str) {
-
+ var code = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q", 
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    a: "n",
+    b: "o",
+    c: "p",
+    d: "q",
+    e: "r",
+    f: "s",
+    g: "t",
+    h: "u",
+    i: "v",
+    j: "w",
+    k: "x",
+    l: "y",
+    m: "z",
+    n: "a",
+    o: "b",
+    p: "c",
+    q: "d",
+    r: "e",
+    s: "f",
+    t: "g",
+    u: "h",
+    v: "i",
+    w: "j",
+    x: "k",
+    y: "l",
+    z: "m"
+  };
+  var splitStr = str.split("");
+  for (var i = 0; i < splitStr.length; i++){
+      for (var prop in code){
+          if (prop === splitStr[i]){
+              splitStr[i] = code[prop];
+             break;
+          }
+      }
+  }
+  var codeman = splitStr.join('');
+  return codeman;
 }
 
 //Input	ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
